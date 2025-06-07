6d8c15bbac5dc080cbeb37b6037987f9205acd20
KlajdiBajo <klajdi.bajo33@gmail.com>
2025-06-07 03:16:03 +0200

    Initial commit

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,40 @@
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+
+interface NavigationItem {
+    name: string;
+    href: string;
+    icon: string;
+}
+
+@Component({
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss'],
+    standalone: true,
+    imports: [CommonModule, RouterModule, IonicModule]
+})
+export class NavbarComponent {
+    isMenuOpen = signal(false);
+
+    navigation: NavigationItem[] = [
+        { name: 'Teachers', href: '/teachers', icon: 'people' },
+        { name: 'Students', href: '/students', icon: 'school' },
+        { name: 'Courses', href: '/courses', icon: 'book' }
+    ];
+
+    toggleMenu() {
+        this.isMenuOpen.update(value => !value);
+    }
+
+    closeMenu(event: Event) {
+        this.isMenuOpen.set(false);
+    }
+
+    isActive(href: string): boolean {
+        const currentPath = window.location.pathname;
+        return currentPath === href || (href === '/teachers' && currentPath === '/');
+    }
+}
